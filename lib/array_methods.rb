ece33e9c4d3cb226511eb05de032a9c4ba2b8844
Andrew-Joseph Blanco <aj.blanco@rocketmail.com>
2019-07-26 04:53:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,11 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.each do |element, index| 
+    if element == value_to_find 
+      index
+    else
+    
+  end
+  value_to_find 
 end
 
 def find_max_value(array)
@@ -8,4 +14,11 @@ end
 
 def find_min_value(array)
   # Add your solution here
+end
+
+array = [1,2,3]
+find_element_index([1,2,3] , 4)
+
+def push 
+  "add to the array"
 end
